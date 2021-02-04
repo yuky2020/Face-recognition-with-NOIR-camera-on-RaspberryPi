b1c8f65e4d95066e9f4bdd4783778e9ff3876249
Matteo <yuky2020@gmail.com>
2021-02-04 17:26:07 +0100

    Working hard to get face rec work properly also on server

--- Server/Server.py
+++ Server/Server.py
@@ -1,5 +1,5 @@
 import pysftp
-
+import face_recognition
 myHostname = "192.168.1.6"
 myUsername = "pi"
 myPassword = "facerec1"
@@ -14,5 +14,12 @@ with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword
             localFilePath = 'unknow/unknow{}.jpg'.format(i)
             #get the file from raspberry
             sftp.get(remoteFilePath, localFilePath)
+            #load the image and extrapolate faces using viola-jones algorithm  
+            uncknow_image = face_recognition.load_image_file("your_file.jpg")
+            face_locations = face_recognition.face_locations(image)
+            #encode the face in a standard format 
+            unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+            #iterate comparison between all saved face 
+
 
 # connection closed automatically at the end of the with-block
