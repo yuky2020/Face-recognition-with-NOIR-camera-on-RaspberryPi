b55782f2eb54c9040d67fd103866d3253976bb5b
Matteo <yuky2020@gmail.com>
2021-02-05 23:04:54 +0100

    Code work very well  now
    Added anti lan problem detection

--- Server/Server.py
+++ Server/Server.py
@@ -15,70 +15,89 @@ knowdirectory = r'known_faces'
 known_faces=[]
 known_faces_encodings=[]
 known_faces_names=[]
+matches=[]
 k=0
 #iterate trouhght all 
 for filename in os.listdir(knowdirectory):
-    know_faces_names[k]=filename
-    known_faces[k]=face_recognition.load_image_file(filename)
-    known_faces_encodings[k]=face_recognition.face_encodings(known_faces[k])[0]
+    known_faces_names.append(filename)
+    known_faces.append(face_recognition.load_image_file("known_faces/{}".format(filename)))
+    known_faces_encodings.append(face_recognition.face_encodings(known_faces[k])[0])
     k=k+1
-
-with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:
-    print("Connection succesfully stablished ... ")
-    while(run):
-        #save the distance of the best result  for evry istance and the index of the know person in the  list identificated,
-        #if the distance of the best result point to the same person and are equal there is probably an attack on the lan
-        # so rise an allarm   
-        savedDistances=[]
-        savedPersonIndex=[]
-        allarmRise=True
-
-        for i in range(5):   
+while(run):
+    #save the distance of the best result  for evry istance and the index of the know person in the  list identificated,
+    #if the distance of the best result point to the same person and are equal there is probably an attack on the lan
+    # so rise an allarm   
+    savedDistances=[]
+    savedPersonIndex=[]
+    allarmRise=False
+    with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:
+        print("Connection succesfully stablished ... ")
+        for i in range(10):   
             # Define the file that you want to download from the raspberry directory
             remoteFilePath = 'photo/image{}.jpg'.format(i)
             # Define the local path where the file will be saved
             localFilePath = 'unknown/unknown{}.jpg'.format(i)
             #get the file from raspberry
             sftp.get(remoteFilePath, localFilePath)
-            #load the image and extrapolate faces in the image  using viola-jones algorithm  
-            unknown_image = face_recognition.load_image_file("unknown/unknown{}".format(i))
-            #Save the location for the varius faces 
-            unknown_face_locations = face_recognition.face_locations(unknown_image)
-            #encode the faces in a standard format 
-            unknown_face_encodings = face_recognition.face_encodings(unknown_image,unknown_face_locations)
-            #Loop through each face found in the unknown image
-            # and operate comparison between encodings
-            for (top, right, bottom, left), face_encoding in zip(unknown_face_locations, unknown_face_encodings):
-                # See if the face is a match for the known faces, (operate comparison between all encodings
-                #using array of true,false value
-                matches = face_recognition.compare_faces(known_faces_encodings[k], face_encoding)
-                 #set the name as unknown
-                name = "Unknown"
-                #use the known face with the smallest distance to the new face as the  valuable one
-                #calcolate array of face distance
-                face_distances = face_recognition.face_distance(known_faces_encodings, face_encoding)
-                #take the best one as the argmin 
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_faces_names[best_match_index]
-                    #add distance  to the distance list 
-                    savedDistances.append(face_distances[best_match_index])
-                    #add index to the index list 
-                    savedPersonIndex.append(best_match_index)
+
+    # connection closed automatically at the end of the with-block
+
+    #there is a file losing during real time transfer, one evry 30 frames usaly
+   
+
+    for i in range(10):
+        try:    
+                #load the image and extrapolate faces in the image  using viola-jones algorithm  
+                unknown_image = face_recognition.load_image_file("unknown/unknown{}.jpg".format(i))
+                #Save the location for the varius faces 
+                unknown_face_locations = face_recognition.face_locations(unknown_image)
+                #encode the faces in a standard format 
+                unknown_face_encodings = face_recognition.face_encodings(unknown_image,unknown_face_locations)
+                #Loop through each face found in the unknown image
+                # and operate comparison between encodings
+                for face_encoding in unknown_face_encodings:
+                    # See if the face is a match for the known faces, (operate comparison between all encodings
+                    #using array of true,false value
+                    matches=(face_recognition.compare_faces(known_faces_encodings, face_encoding))
+                    #set the name as unknown
+                    name = "Unknown"
+                    #use the known face with the smallest distance to the new face as the  valuable one
+                    #calcolate array of face distance
+                    face_distances = face_recognition.face_distance(known_faces_encodings, face_encoding)
+                    #take the best one as the argmin 
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = known_faces_names[best_match_index]
+                        #add distance  to the distance list 
+                        savedDistances.append(face_distances[best_match_index])
+                        #add index to the index list 
+                        savedPersonIndex.append(best_match_index)
+
                     #if a person is found print it .
                     print("found a person named {}".format(name))
 
-        for i in savedPersonIndex:
-            if(savedPersonIndex[i]!=savedPersonIndex[i+1] or savedDistances[i]!=savedDistances[i+1]):
-                allarmRise=False 
-                
-        if(allarmRise):
-            #close the server  
-            print("lan is compromised the server shotdown for security reason ")
-            run=False        
+        except: print("one image is lost during the tranfert")
+
+
+
+#if there is more than 8 equal result there is probably a lan introduction       
+if len(savedPersonIndex)>=8:
+    #check if lan is exoposed
+    lanex=True
+    for i in savedPersonIndex:
+        for j in savedPersonIndex:
+            if(savedPersonIndex[i]!=savedPersonIndex[j] or savedDistances[i]!=savedDistances[j]):
+                lanex=False
+    #if the lan is exposed kill the process
+    if lanex:allarmRise=True
+
+    if(allarmRise):
+        #close the server  
+        print("lan is compromised the server shotdown for security reason ")
+        run=False        
 
 
 
 
 
-# connection closed automatically at the end of the with-block
+
--- piclient/client.py
+++ piclient/client.py
@@ -2,15 +2,22 @@
 The following code will import PiCamera module, snap 5 pictures and save it in a directory you specify, wait  2 second  and start again.
 the fotoresistor meanwhile will set the ir led to needed light emmission. 
 '''
-from picamera import PiCamera
+import picamera
 from time import sleep
-camera = PiCamera()
+camera = picamera.PiCamera()
+camera.resolution=(320,240)
+
 while True:
     camera.start_preview()
     sleep(1)
     camera.capture('photo/image0.jpg')
+    camera.capture('photo/image1.jpg')
     camera.capture('photo/image2.jpg')
     camera.capture('photo/image3.jpg')
     camera.capture('photo/image4.jpg')
     camera.capture('photo/image5.jpg')
+    camera.capture('photo/image6.jpg')
+    camera.capture('photo/image7.jpg')
+    camera.capture('photo/image8.jpg')
+    camera.capture('photo/image9.jpg')
     camera.stop_preview()
